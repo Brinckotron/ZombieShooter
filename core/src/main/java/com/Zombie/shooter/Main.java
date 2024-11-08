24b23c6cdddb3a06299e93b84ff29732e40c7b45
François Brinck-Boucher <146009172+Brinckotron@users.noreply.github.com>
2024-11-07 19:44:23 -0500

    SpawnZombies and Draw Logic

--- core/src/main/java/com/Zombie/shooter/Main.java
+++ core/src/main/java/com/Zombie/shooter/Main.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
@@ -20,6 +21,7 @@ public class Main implements ApplicationListener {
     Texture backgroundTexture;
     Texture heroTexture;
     Texture zombieTexture;
+    Texture bulletTexture;
     Texture tower;
     Texture bullets;
     Sound gunSound;
@@ -47,13 +49,32 @@ public class Main implements ApplicationListener {
     @Override
     public void create() {
         backgroundTexture = new Texture("Background.png");
+        heroTexture = new Texture("Hero.png");
+        zombieTexture = new Texture("Zombie.png");
+        bulletTexture = new Texture("Bullet.png");
 
+        //zombieSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
+        //music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
 
         spriteBatch = new SpriteBatch();
         viewport = new FitViewport(896, 512);
 
+        heroSprite = new Sprite(heroTexture);
+        heroSprite.setSize(25, 25);
 
         touchPos = new Vector2();
+
+        zombieSprites = new Array<>();
+        bulletSprites = new Array<>();
+
+        heroRectangle = new Rectangle();
+        zombieRectangles = new Array<>();
+
+        spawnZombie();
+
+       /* music.setLooping(true);
+        music.setVolume(.5f);
+        music.play();*/
     }
 
     @Override
@@ -88,7 +109,31 @@ public class Main implements ApplicationListener {
         float worldHeight = viewport.getWorldHeight();
 
         spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight);
+        heroSprite.draw(spriteBatch);
+
+        for (Sprite zombieSprite : zombieSprites) {
+            zombieSprite.draw(spriteBatch);
+        }
+
+        for (Sprite bulletSprite : bulletSprites) {
+            bulletSprite.draw(spriteBatch);
+        }
+
         spriteBatch.end();
+    }
+
+    private void spawnZombie() {
+
+        float zombieWidth = 25;
+        float zombieHeight = 25;
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        Sprite zombieSprite = new Sprite(zombieTexture);
+        zombieSprite.setSize(zombieWidth, zombieHeight);
+        zombieSprite.setX(MathUtils.random(0, 1) == 0? 0f: worldWidth - zombieWidth);
+        zombieSprite.setY(MathUtils.random(0f, worldHeight - zombieHeight));
+        zombieSprites.add(zombieSprite);
     }
 
 
